6a57283f4e22c070210888d501285684650c9250
Chris Martinez <chris.martinez@microsoft.com>
2016-08-31 21:47:30 -0700

    Fixed the internal action selection process to consider both convention and attributing routing using two passes when needed.

---
+++ src/Microsoft.AspNet.WebApi.Versioning/Controllers/ActionSelectionResult.cs
@@ -0,0 +1,33 @@
+﻿namespace Microsoft.Web.Http.Controllers
+{
+    using System;
+    using System.Diagnostics.Contracts;
+    using System.Web.Http.Controllers;
+
+    /// <content>
+    /// Provides additional content for the <see cref="ApiVersionActionSelector"/> class.
+    /// </content>
+    public partial class ApiVersionActionSelector
+    {
+        private sealed class ActionSelectionResult
+        {
+            internal ActionSelectionResult( HttpActionDescriptor action )
+            {
+                Contract.Requires( action != null );
+                Action = action;
+            }
+
+            internal ActionSelectionResult( Exception exception )
+            {
+                Contract.Requires( exception != null );
+                Exception = exception;
+            }
+
+            internal bool Succeeded => Exception == null;
+
+            internal HttpActionDescriptor Action { get; }
+
+            internal Exception Exception { get; }
+        }
+    }
+}
