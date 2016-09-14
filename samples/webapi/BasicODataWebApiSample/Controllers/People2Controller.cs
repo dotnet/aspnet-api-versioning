33be1f71a0ec0457f6fcccbc3073a2e8b040c92b
Chris Martinez <chris.martinez@microsoft.com>
2016-09-14 11:13:55 -0700

    Update the sample projects to align with the RTW release

--- samples/webapi/BasicODataWebApiSample/Controllers/People2Controller.cs
+++ samples/webapi/BasicODataWebApiSample/Controllers/People2Controller.cs
@@ -2,7 +2,6 @@
 {
     using Microsoft.Web.Http;
     using Models;
-    using System.Threading.Tasks;
     using System.Web.Http;
     using System.Web.OData;
     using System.Web.OData.Query;
@@ -13,12 +12,14 @@
     [ODataRoutePrefix( "People" )]
     public class People2Controller : ODataController
     {
-        // GET ~/people?api-version=3.0
+        // GET ~/v3/people
+        // GET ~/api/people?api-version=3.0
         [ODataRoute]
         public IHttpActionResult Get( ODataQueryOptions<Person> options ) =>
             Ok( new[] { new Person() { Id = 1, FirstName = "Bill", LastName = "Mei", Email = "bill.mei@somewhere.com", Phone = "555-555-5555" } } );
 
-        // GET ~/people(1)?api-version=3.0
+        // GET ~/v3/people(1)
+        // GET ~/api/people(1)?api-version=3.0
         [ODataRoute( "({key})" )]
         public IHttpActionResult Get( [FromODataUri] int id, ODataQueryOptions<Person> options ) =>
             Ok( new Person() { Id = id, FirstName = "Bill", LastName = "Mei", Email = "bill.mei@somewhere.com", Phone = "555-555-5555" } );
--- samples/webapi/BasicODataWebApiSample/Controllers/PeopleController.cs
+++ samples/webapi/BasicODataWebApiSample/Controllers/PeopleController.cs
@@ -2,7 +2,6 @@
 {
     using Microsoft.Web.Http;
     using Models;
-    using System.Threading.Tasks;
     using System.Web.Http;
     using System.Web.OData;
     using System.Web.OData.Query;
@@ -14,18 +13,21 @@
     public class PeopleController : ODataController
     {
         // GET ~/v1/people
-        // GET ~/people?api-version=[1.0|2.0]
+        // GET ~/v2/people
+        // GET ~/api/people?api-version=[1.0|2.0]
         [ODataRoute]
         public IHttpActionResult Get( ODataQueryOptions<Person> options ) =>
             Ok( new[] { new Person() { Id = 1, FirstName = "Bill", LastName = "Mei", Email = "bill.mei@somewhere.com", Phone = "555-555-5555" } } );
 
         // GET ~/v1/people(1)
-        // GET ~/people(1)?api-version=[1.0|2.0]
+        // GET ~/v2/people(1)
+        // GET ~/api/people(1)?api-version=[1.0|2.0]
         [ODataRoute( "({id})" )]
         public IHttpActionResult Get( [FromODataUri] int id, ODataQueryOptions<Person> options ) =>
             Ok( new Person() { Id = id, FirstName = "Bill", LastName = "Mei", Email = "bill.mei@somewhere.com", Phone = "555-555-5555" } );
 
-        // PATCH ~/people(1)?api-version=2.0
+        // PATCH ~/v2/people(1)
+        // PATCH ~/api/people(1)?api-version=2.0
         [MapToApiVersion( "2.0" )]
         [ODataRoute( "({id})" )]
         public IHttpActionResult Patch( [FromODataUri] int id, Delta<Person> delta, ODataQueryOptions<Person> options )
