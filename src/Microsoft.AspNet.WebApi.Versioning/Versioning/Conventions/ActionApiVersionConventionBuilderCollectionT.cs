bc5b45250054f2114653c53936c149d8d0a0b355
Chris Martinez <chris.martinez@microsoft.com>
2016-11-09 10:04:54 -0800

    Refactored the way API version conventions are mapped to controller actions, which also resolves a few flaky tests. Fixes #55.

---
+++ src/Microsoft.AspNet.WebApi.Versioning/Versioning/Conventions/ActionApiVersionConventionBuilderCollectionT.cs
@@ -0,0 +1,16 @@
+﻿namespace Microsoft.Web.Http.Versioning.Conventions
+{
+    using System;
+    using System.Web.Http.Controllers;
+
+    /// <content>
+    /// Provides additional implementation specific to Microsoft ASP.NET Web API.
+    /// </content>
+    /// <typeparam name="T">The <see cref="Type">type</see> of <see cref="IHttpController">controller</see>.</typeparam>
+    public partial class ActionApiVersionConventionBuilderCollection<T> where T : IHttpController
+    {
+        private sealed partial class ActionBuilderMapping<TModel> where TModel : IHttpController
+        {
+        }
+    }
+}
