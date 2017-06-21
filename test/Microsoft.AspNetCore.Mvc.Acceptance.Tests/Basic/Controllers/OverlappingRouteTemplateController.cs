991edb59dfad5af932bfdb725ba25832209c8204
Chris Martinez <chris.martinez@microsoft.com>
2017-06-21 14:36:40 -0700

    Update action selection for routes with overlapping route templates. Fixes #148. Relates to #147.

---
+++ test/Microsoft.AspNetCore.Mvc.Acceptance.Tests/Basic/Controllers/OverlappingRouteTemplateController.cs
@@ -0,0 +1,16 @@
+﻿namespace Microsoft.AspNetCore.Mvc.Basic.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using System;
+
+    [ApiVersion( "1.0" )]
+    [Route( "api/v{version:apiVersion}/values" )]
+    public class OverlappingRouteTemplateController : Controller
+    {
+        [HttpGet( "{id:int}/{childId}" )]
+        public IActionResult Get( int id, string childId ) => Ok( new { id, childId } );
+
+        [HttpGet( "{id:int}/children" )]
+        public IActionResult Get( int id ) => Ok( new { id } );
+    }
+}
