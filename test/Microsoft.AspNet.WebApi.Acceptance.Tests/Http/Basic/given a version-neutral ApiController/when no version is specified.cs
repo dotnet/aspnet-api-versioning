fc0ebbba3b3c38869e5bbd2507da210806e42043
Chris Martinez <chris.martinez@microsoft.com>
2017-07-06 15:55:40 -0700

    Update 405 handling for API version-neutral routes. Fixes #159

--- test/Microsoft.AspNet.WebApi.Acceptance.Tests/Http/Basic/given a version-neutral ApiController/when no version is specified.cs
+++ test/Microsoft.AspNet.WebApi.Acceptance.Tests/Http/Basic/given a version-neutral ApiController/when no version is specified.cs
@@ -1,7 +1,9 @@
 ﻿namespace given_a_version_neutral_ApiController
 {
     using FluentAssertions;
+    using Microsoft.Web;
     using Microsoft.Web.Http.Basic;
+    using System.Net.Http;
     using System.Threading.Tasks;
     using Xunit;
     using static System.Net.HttpStatusCode;
@@ -19,6 +21,32 @@
 
             // assert
             response.StatusCode.Should().Be( NoContent );
+        }
+
+        [Fact]
+        public async Task then_post_should_return_405()
+        {
+            // arrange
+            var entity = new { };
+
+            // act
+            var response = await PostAsync( "api/ping", entity );
+            var content = await response.Content.ReadAsAsync<OneApiErrorResponse>();
+
+            // assert
+            response.StatusCode.Should().Be( MethodNotAllowed );
+            response.Content.Headers.Allow.Should().BeEquivalentTo( "GET" );
+            content.Error.ShouldBeEquivalentTo(
+                new
+                {
+                    Code = "UnsupportedApiVersion",
+                    InnerError = new
+                    {
+                        Code = default( string ),
+                        Message = "No route providing a controller name with API version '(null)' was found to match request URI 'http://localhost/api/ping'."
+                    },
+                    Message = "The HTTP resource that matches the request URI 'http://localhost/api/ping' does not support the API version '(null)'."
+                } );
         }
     }
 }
--- test/Microsoft.AspNetCore.Mvc.Acceptance.Tests/Basic/given a version-neutral Controller/when no version is specified.cs
+++ test/Microsoft.AspNetCore.Mvc.Acceptance.Tests/Basic/given a version-neutral Controller/when no version is specified.cs
@@ -1,7 +1,9 @@
 ﻿namespace given_a_versionX2Dneutral_Controller
 {
     using FluentAssertions;
+    using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.Mvc.Basic;
+    using System.Net.Http;
     using System.Threading.Tasks;
     using Xunit;
     using static System.Net.HttpStatusCode;
@@ -19,6 +21,28 @@
 
             // assert
             response.StatusCode.Should().Be( NoContent );
+        }
+
+        [Fact]
+        public async Task then_post_should_return_405()
+        {
+            // arrange
+            var entity = new { };
+
+            // act
+            var response = await PostAsync( "api/ping", entity );
+            var content = await response.Content.ReadAsAsync<OneApiErrorResponse>();
+
+            // assert
+            response.StatusCode.Should().Be( MethodNotAllowed );
+            response.Content.Headers.Allow.Should().BeEquivalentTo( "GET" );
+            content.Error.ShouldBeEquivalentTo(
+                new
+                {
+                    Code = "UnsupportedApiVersion",
+                    InnerError = default( OneApiInnerError ),
+                    Message = "The HTTP resource that matches the request URI 'http://localhost/api/ping' is not supported."
+                } );
         }
     }
 }
