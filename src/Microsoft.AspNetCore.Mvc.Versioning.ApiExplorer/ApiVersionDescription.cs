6c200f84091031ddd78444b3494d4d681d290cf3
Alexander Tsoi <aliaksandr.tsoi@gmail.com>
2018-06-20 17:12:14 -0400

    Fix a typo in xml docs for ApiVersion

--- src/Microsoft.AspNetCore.Mvc.Versioning.ApiExplorer/ApiVersionDescription.cs
+++ src/Microsoft.AspNetCore.Mvc.Versioning.ApiExplorer/ApiVersionDescription.cs
@@ -27,7 +27,7 @@
         public ApiVersion ApiVersion { get; }
 
         /// <summary>
-        /// Gets the API version group name.f
+        /// Gets the API version group name.
         /// </summary>
         /// <value>The group name for the API version.</value>
         public string GroupName { get; }
