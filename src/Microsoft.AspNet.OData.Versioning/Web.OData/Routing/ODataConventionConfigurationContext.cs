5408a4a26b21ec2e7267a01c2083c77b36b2092e
Chris Martinez <chris.martinez@microsoft.com>
2018-07-01 11:47:14 -0700

    Improve support to configure routing conventions. Closes #308.

---
+++ src/Microsoft.AspNet.OData.Versioning/Web.OData/Routing/ODataConventionConfigurationContext.cs
@@ -0,0 +1,67 @@
+﻿namespace Microsoft.Web.OData.Routing
+{
+    using Microsoft.OData.Edm;
+    using Microsoft.Web.Http;
+    using System.Collections.Generic;
+    using System.Web.Http;
+    using System.Web.OData.Routing.Conventions;
+
+    /// <summary>
+    /// Represents the context used to configure OData routing conventions.
+    /// </summary>
+    public class ODataConventionConfigurationContext
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ODataConventionConfigurationContext"/> class.
+        /// </summary>
+        /// <param name="configuration">The current <see cref="HttpConfiguration">configuration</see>.</param>
+        /// <param name="routeName">The current route name.</param>
+        /// <param name="edmModel">The current <see cref="IEdmModel">EDM model</see>.</param>
+        /// <param name="apiVersion">The current <see cref="ApiVersion">API version</see>.</param>
+        /// <param name="routingConventions">The initial <see cref="IList{T}">list</see> of <see cref="IODataRoutingConvention">routing conventions</see>.</param>
+        public ODataConventionConfigurationContext( HttpConfiguration configuration, string routeName, IEdmModel edmModel, ApiVersion apiVersion, IList<IODataRoutingConvention> routingConventions )
+        {
+            Arg.NotNull( configuration, nameof( configuration ) );
+            Arg.NotNullOrEmpty( routeName, nameof( routeName ) );
+            Arg.NotNull( edmModel, nameof( edmModel ) );
+            Arg.NotNull( apiVersion, nameof( apiVersion ) );
+            Arg.NotNull( routingConventions, nameof( routingConventions ) );
+
+            Configuration = configuration;
+            RouteName = routeName;
+            EdmModel = edmModel;
+            ApiVersion = ApiVersion;
+            RoutingConventions = routingConventions;
+        }
+
+        /// <summary>
+        /// Gets the configuration associated with the routing conventions.
+        /// </summary>
+        /// <value>The current <see cref="HttpConfiguration">configuration</see>.</value>
+        public HttpConfiguration Configuration { get; }
+
+        /// <summary>
+        /// Gets the name of the route the conventions are being created for.
+        /// </summary>
+        /// <value>The current route name.</value>
+        public string RouteName { get; }
+
+        /// <summary>
+        /// Gets the entity data model (EDM) associcated with the routing conventions.
+        /// </summary>
+        /// <value>The current <see cref="IEdmModel">EDM model</see>.</value>
+        public IEdmModel EdmModel { get; }
+
+        /// <summary>
+        /// Gets the API version associated with the routing conventions.
+        /// </summary>
+        /// <value>The current <see cref="ApiVersion">API version</see>.</value>
+        public ApiVersion ApiVersion { get; }
+
+        /// <summary>
+        /// Gets the initial list of routing conventions.
+        /// </summary>
+        /// <value>The initial <see cref="IList{T}">list</see> of <see cref="IODataRoutingConvention">routing conventions</see>.</value>
+        public IList<IODataRoutingConvention> RoutingConventions { get; }
+    }
+}
---
+++ src/Microsoft.AspNet.OData.Versioning/Web.OData/Routing/VersionedODataRoutingConventions.cs
@@ -0,0 +1,64 @@
+﻿namespace Microsoft.Web.OData.Routing
+{
+    using System.Collections.Generic;
+    using System.Diagnostics.Contracts;
+    using System.Web.OData.Routing.Conventions;
+
+    /// <summary>
+    /// Provides utility functions to create OData routing conventions with support for API versioning.
+    /// </summary>
+    public static class VersionedODataRoutingConventions
+    {
+        /// <summary>
+        /// Creates a mutable list of the default OData routing conventions with support for API versioning.
+        /// </summary>
+        /// <returns>A mutable <see cref="IList{T}">list</see> of the default <see cref="IODataRoutingConvention">OData routing conventions</see>.</returns>
+        public static IList<IODataRoutingConvention> CreateDefault() => EnsureConventions( ODataRoutingConventions.CreateDefault() );
+
+        /// <summary>
+        /// Adds or updates the specified list of OData routing conventions to ensure the necessary API versioning conventions are present.
+        /// </summary>
+        /// <param name="routingConventions">The <see cref="IList{T}"/> of <see cref="IODataRoutingConvention">OData routing conventions</see> to add to or update.</param>
+        /// <returns>The original <see cref="IList{T}">list</see> of <see cref="IODataRoutingConvention">OData routing conventions</see>,
+        /// possibly with new or updated <see cref="IODataRoutingConvention">OData routing conventions</see>.</returns>
+        public static IList<IODataRoutingConvention> AddOrUpdate( IList<IODataRoutingConvention> routingConventions )
+        {
+            Arg.NotNull( routingConventions, nameof( routingConventions ) );
+            return EnsureConventions( routingConventions );
+        }
+
+        static IList<IODataRoutingConvention> EnsureConventions( IList<IODataRoutingConvention> conventions )
+        {
+            Contract.Requires( conventions != null );
+            Contract.Ensures( Contract.Result<IList<IODataRoutingConvention>>() != null );
+
+            var hasVersionedMetadataConvention = false;
+
+            for ( var i = conventions.Count - 1; i >= 0; i-- )
+            {
+                var convention = conventions[i];
+
+                if ( convention is AttributeRoutingConvention && convention.GetType().Equals( typeof( AttributeRoutingConvention ) ) )
+                {
+                    conventions.RemoveAt( i );
+                }
+                else if ( convention is MetadataRoutingConvention )
+                {
+                    conventions[i] = new VersionedMetadataRoutingConvention();
+                    hasVersionedMetadataConvention = true;
+                }
+                else if ( convention is VersionedMetadataRoutingConvention )
+                {
+                    hasVersionedMetadataConvention = true;
+                }
+            }
+
+            if ( !hasVersionedMetadataConvention )
+            {
+                conventions.Insert( 0, new VersionedMetadataRoutingConvention() );
+            }
+
+            return conventions;
+        }
+    }
+}
