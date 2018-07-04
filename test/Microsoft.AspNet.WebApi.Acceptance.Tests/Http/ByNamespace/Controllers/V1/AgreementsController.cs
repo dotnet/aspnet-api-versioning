395dacfbcba1aacfea1c6fa26d9adbce4d2f0113
Chris Martinez <chris.martinez@microsoft.com>
2018-07-04 13:21:11 -0700

    Update by namespace acceptance tests to use new convention

--- test/Microsoft.AspNet.WebApi.Acceptance.Tests/Http/ByNamespace/Controllers/V1/AgreementsController.cs
+++ test/Microsoft.AspNet.WebApi.Acceptance.Tests/Http/ByNamespace/Controllers/V1/AgreementsController.cs
@@ -4,7 +4,6 @@
     using Models;
     using System.Web.Http;
 
-    [ApiVersion( "1.0" )]
     public class AgreementsController : ApiController
     {
         public IHttpActionResult Get( string accountId ) => Ok( new Agreement( GetType().FullName, accountId, Request.GetRequestedApiVersion().ToString() ) );
