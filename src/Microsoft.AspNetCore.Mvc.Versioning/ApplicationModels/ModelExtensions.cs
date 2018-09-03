807b876bf81f78b2e2115f4a62c654886b7adaff
Chris Martinez <chris.martinez@microsoft.com>
2018-09-03 13:03:15 -0700

    Simplify extension methods

--- src/Microsoft.AspNetCore.Mvc.Versioning/ApplicationModels/ModelExtensions.cs
+++ src/Microsoft.AspNetCore.Mvc.Versioning/ApplicationModels/ModelExtensions.cs
@@ -1,8 +1,6 @@
 ﻿namespace Microsoft.AspNetCore.Mvc.ApplicationModels
 {
     using System;
-    using System.Linq;
-    using Microsoft.AspNetCore.Mvc.Versioning;
 
     /// <summary>
     /// Provides extension methods for <see cref="ApplicationModel">application models</see>, <see cref="ControllerModel">controller models</see>,
@@ -17,7 +15,7 @@
         /// <typeparam name="T">The <see cref="Type">type</see> and key of the property.</typeparam>
         /// <param name="controller">The <see cref="ControllerModel">model</see> to get the property from.</param>
         /// <returns>The property value of <typeparamref name="T"/> or its default value.</returns>
-        public static T GetProperty<T>( this ControllerModel controller ) where T : class
+        public static T GetProperty<T>( this ControllerModel controller )
         {
             Arg.NotNull( controller, nameof( controller ) );
             return controller.Properties.GetOrDefault( typeof( T ), default( T ) );
@@ -29,7 +27,7 @@
         /// <typeparam name="T">The <see cref="Type">type</see> and key of the property.</typeparam>
         /// <param name="controller">The <see cref="ControllerModel">model</see> to set the property for.</param>
         /// <param name="value">The property value to set.</param>
-        public static void SetProperty<T>( this ControllerModel controller, T value ) where T : class
+        public static void SetProperty<T>( this ControllerModel controller, T value )
         {
             Arg.NotNull( controller, nameof( controller ) );
             controller.Properties.SetOrRemove( typeof( T ), value );
@@ -41,7 +39,7 @@
         /// <typeparam name="T">The <see cref="Type">type</see> and key of the property.</typeparam>
         /// <param name="action">The <see cref="ActionModel">model</see> to get the property from.</param>
         /// <returns>The property value of <typeparamref name="T"/> or its default value.</returns>
-        public static T GetProperty<T>( this ActionModel action ) where T : class
+        public static T GetProperty<T>( this ActionModel action )
         {
             Arg.NotNull( action, nameof( action ) );
             return action.Properties.GetOrDefault( typeof( T ), default( T ) );
@@ -53,10 +51,14 @@
         /// <typeparam name="T">The <see cref="Type">type</see> and key of the property.</typeparam>
         /// <param name="action">The <see cref="ActionModel">model</see> to set the property for.</param>
         /// <param name="value">The property value to set.</param>
-        public static void SetProperty<T>( this ActionModel action, T value ) where T : class
+        public static void SetProperty<T>( this ActionModel action, T value )
         {
             Arg.NotNull( action, nameof( action ) );
             action.Properties.SetOrRemove( typeof( T ), value );
         }
+
+#pragma warning disable CA1801 // Review unused parameters; intentional for type parameter
+        internal static void RemoveProperty<T>( this ActionModel action, T value ) => action.Properties.Remove( typeof( T ) );
+#pragma warning restore CA1801
     }
 }
