71c2affdaed2bed6d1226577db8cdf8edf492044
LuukN2 <lw.naaijen@student.avans.nl>
2018-10-29 15:51:37 +0100

    Support substitution of types with circular references. Replaced tuple with PropertyDependency.

--- src/Common.OData.ApiExplorer/AspNet.OData/PropertyDependency.cs
+++ src/Common.OData.ApiExplorer/AspNet.OData/PropertyDependency.cs
@@ -10,26 +10,23 @@ namespace Microsoft.AspNet.OData
         /// <summary>
         /// Initializes a new instance of the <see cref="PropertyDependency"/> class.
         /// </summary>
-        /// <param name="dependentType">The type that contains the dependent property.</param>
         /// <param name="dependentOnTypeKey">The key of the type the property has a dependency on.</param>
         /// <param name="propertyName">The name of the property.</param>
         /// <param name="isCollection">Whether the property is a collection or not.</param>
-        internal PropertyDependency( TypeBuilder dependentType, EdmTypeKey dependentOnTypeKey,  bool isCollection, string propertyName )
+        internal PropertyDependency( EdmTypeKey dependentOnTypeKey,  bool isCollection, string propertyName )
         {
-            Arg.NotNull<TypeBuilder>( dependentType, nameof(dependentType) );
             Arg.NotNull<EdmTypeKey>( dependentOnTypeKey, nameof( dependentOnTypeKey ) );
             Arg.NotNull<string>( propertyName, nameof( propertyName ) );
 
-            DependentType = dependentType;
             DependentOnTypeKey = dependentOnTypeKey;
             PropertyName = propertyName;
             IsCollection = isCollection;
         }
 
         /// <summary>
-        /// Gets the type that has a dependent property.
+        /// Gets or sets the type that has a dependent property.
         /// </summary>
-        internal TypeBuilder DependentType { get; }
+        internal TypeBuilder DependentType { get; set;  }
 
         /// <summary>
         /// Gets the key of the type the property has a dependency on.
