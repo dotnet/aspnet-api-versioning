32c370069f0559268d43a92348eb17d89e0af35e
Chris Martinez <chris.martinez@microsoft.com>
2018-12-14 13:02:09 -0800

    Respect [ApiExplorerSettings(IgnoreApi = false)]. Fixes #405.

---
+++ src/Microsoft.AspNetCore.OData.Versioning/AspNetCore.Mvc/ApplicationModels/ApiExplorerModelExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace Microsoft.AspNetCore.Mvc.ApplicationModels
+{
+    using System;
+
+    /// <summary>
+    /// Provides extension methods for the <see cref="ApiExplorerModel"/> class.
+    /// </summary>
+    [CLSCompliant( false )]
+    public static class ApiExplorerModelExtensions
+    {
+        /// <summary>
+        /// Returns a value indicating whether the specified model is visible to OData.
+        /// </summary>
+        /// <param name="model">The <see cref="ApiExplorerModel">model</see> to evaluate.</param>
+        /// <returns>True if the associated controller or action is visible to OData; otherwise, false.</returns>
+        public static bool? IsODataVisible( this ApiExplorerModel model )
+        {
+            Arg.NotNull( model, nameof( model ) );
+            return model is ODataApiExplorerModel odataModel ? odataModel.IsODataVisible : model.IsVisible;
+        }
+    }
+}
