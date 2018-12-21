8566f26295173fa838bf84d681109dee0c4e6eae
Chris Martinez <chris.martinez@microsoft.com>
2018-12-21 15:48:54 -0800

    Remove MVC UI compilation setup for tests; it's no longer needed

--- test/Microsoft.AspNetCore.Mvc.Acceptance.Tests/Mvc/Basic/UIFixture.cs
+++ test/Microsoft.AspNetCore.Mvc.Acceptance.Tests/Mvc/Basic/UIFixture.cs
@@ -1,13 +1,7 @@
 ﻿namespace Microsoft.AspNetCore.Mvc.Basic
 {
     using Microsoft.AspNetCore.Mvc.ApplicationParts;
-    using Microsoft.AspNetCore.Mvc.Razor;
     using Microsoft.AspNetCore.Mvc.Versioning;
-    using Microsoft.Extensions.DependencyInjection;
-    using System.Linq;
-    using System.Reflection;
-    using static Microsoft.CodeAnalysis.MetadataReference;
-    using static System.Reflection.Assembly;
 
     public class UIFixture : HttpServerFixture
     {
@@ -17,28 +11,6 @@
         {
             partManager.FeatureProviders.Add( (IApplicationFeatureProvider) FilteredControllerTypes );
             partManager.ApplicationParts.Add( new AssemblyPart( GetType().Assembly ) );
-        }
-
-        protected override void OnConfigureServices( IServiceCollection services )
-        {
-            services.Configure<RazorViewEngineOptions>( options =>
-            {
-                options.CompilationCallback += context =>
-                {
-                    var assembly = GetType().GetTypeInfo().Assembly;
-                    var assemblies = assembly.GetReferencedAssemblies().Select( x => CreateFromFile( Load( x ).Location ) ).ToList();
-
-                    assemblies.Add( CreateFromFile( Load( new AssemblyName( "mscorlib" ) ).Location ) );
-#if NET461
-                    assemblies.Add( CreateFromFile( Load( new AssemblyName( "netstandard" ) ).Location ) );
-#else
-                    assemblies.Add( CreateFromFile( Load( new AssemblyName( "System.Private.Corelib" ) ).Location ) );
-#endif
-                    assemblies.Add( CreateFromFile( Load( new AssemblyName( "System.Dynamic.Runtime" ) ).Location ) );
-                    assemblies.Add( CreateFromFile( Load( new AssemblyName( "Microsoft.AspNetCore.Razor" ) ).Location ) );
-                    context.Compilation = context.Compilation.AddReferences( assemblies );
-                };
-            } );
         }
     }
 }
