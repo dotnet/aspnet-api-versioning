ccd65eca552f9bcd9a14c8cffe2042d38cd7d2f1
Xavier <xavierjohn@hotmail.com>
2021-03-03 12:08:44 -0800

    Sample: Single controller with multiple versions.

---
+++ samples/aspnetcore/BasicSample/Controllers/MultiVersionedController.cs
@@ -0,0 +1,18 @@
+﻿namespace Microsoft.Examples.Controllers
+{
+    using AspNetCore.Routing;
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [ApiVersion( "1.0" )]
+    [ApiVersion( "2.0" )]
+    [Route( "api/v{version:apiVersion}/[controller]" )]
+    public class MultiVersionedController : ControllerBase
+    {
+        [HttpGet]
+        public string GetMe() => "Version 1";
+
+        [HttpGet, MapToApiVersion( "2.0" )]
+        public string GetMeV2() => "Version 2";
+    }
+}
