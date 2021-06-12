7b5bb14e186dc2098f8d70c45702ecae2a626df6
Chris Martinez <sydefekt@hotmail.com>
2021-06-12 11:40:24 -0700

    Clear API versioning feature on reentry. Fixes #749. Resolves #748

--- src/Microsoft.AspNetCore.Mvc.Versioning/Versioning/ApiVersioningMiddleware.cs
+++ src/Microsoft.AspNetCore.Mvc.Versioning/Versioning/ApiVersioningMiddleware.cs
@@ -3,13 +3,19 @@
 namespace Microsoft.AspNetCore.Mvc.Versioning
 {
     using Microsoft.AspNetCore.Http;
+    using Microsoft.Extensions.Options;
     using System.Threading.Tasks;
 
     sealed class ApiVersioningMiddleware
     {
         readonly RequestDelegate next;
+        readonly bool usingLegacyRouting;
 
-        public ApiVersioningMiddleware( RequestDelegate next ) => this.next = next;
+        public ApiVersioningMiddleware( RequestDelegate next, IOptions<MvcOptions> options )
+        {
+            this.next = next;
+            usingLegacyRouting = !options.Value.EnableEndpointRouting;
+        }
 
         public Task InvokeAsync( HttpContext context )
         {
@@ -19,6 +25,15 @@ namespace Microsoft.AspNetCore.Mvc.Versioning
             {
                 feature = new ApiVersioningFeature( context );
                 context.Features.Set( feature );
+            }
+            else
+            {
+                feature.RouteParameter = null;
+
+                if ( usingLegacyRouting )
+                {
+                    feature.SelectionResult.Clear();
+                }
             }
 
             return next( context );
