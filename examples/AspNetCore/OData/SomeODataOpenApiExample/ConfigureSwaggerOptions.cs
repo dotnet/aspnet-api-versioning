e0fd91bda164c24d6bbb4f63a7b85ebf3451bd23
Chris Martinez <sydefekt@hotmail.com>
2022-05-04 14:38:12 -0700

    Add examples for using 'some' OData with OpenAPI

---
+++ examples/AspNetCore/OData/SomeODataOpenApiExample/ConfigureSwaggerOptions.cs
@@ -0,0 +1,89 @@
+﻿namespace ApiVersioning.Examples;
+
+using Asp.Versioning;
+using Asp.Versioning.ApiExplorer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Text;
+
+/// <summary>
+/// Configures the Swagger generation options.
+/// </summary>
+/// <remarks>This allows API versioning to define a Swagger document per API version after the
+/// <see cref="IApiVersionDescriptionProvider"/> service has been resolved from the service container.</remarks>
+public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
+{
+    private readonly IApiVersionDescriptionProvider provider;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ConfigureSwaggerOptions"/> class.
+    /// </summary>
+    /// <param name="provider">The <see cref="IApiVersionDescriptionProvider">provider</see> used to generate Swagger documents.</param>
+    public ConfigureSwaggerOptions( IApiVersionDescriptionProvider provider ) => this.provider = provider;
+
+    /// <inheritdoc />
+    public void Configure( SwaggerGenOptions options )
+    {
+        // add a swagger document for each discovered API version
+        // note: you might choose to skip or document deprecated API versions differently
+        foreach ( var description in provider.ApiVersionDescriptions )
+        {
+            options.SwaggerDoc( description.GroupName, CreateInfoForApiVersion( description ) );
+        }
+    }
+
+    private static OpenApiInfo CreateInfoForApiVersion( ApiVersionDescription description )
+    {
+        var text = new StringBuilder( "An example application with some OData, OpenAPI, Swashbuckle, and API versioning." );
+        var info = new OpenApiInfo()
+        {
+            Title = "Sample API",
+            Version = description.ApiVersion.ToString(),
+            Contact = new OpenApiContact() { Name = "Bill Mei", Email = "bill.mei@somewhere.com" },
+            License = new OpenApiLicense() { Name = "MIT", Url = new Uri( "https://opensource.org/licenses/MIT" ) }
+        };
+
+        if ( description.IsDeprecated )
+        {
+            text.Append( " This API version has been deprecated." );
+        }
+
+        if ( description.SunsetPolicy is SunsetPolicy policy )
+        {
+            if ( policy.Date is DateTimeOffset when )
+            {
+                text.Append( " The API will be sunset on " )
+                    .Append( when.Date.ToShortDateString() )
+                    .Append( '.' );
+            }
+
+            if ( policy.HasLinks )
+            {
+                text.AppendLine();
+
+                for ( var i = 0; i < policy.Links.Count; i++ )
+                {
+                    var link = policy.Links[i];
+
+                    if ( link.Type == "text/html" )
+                    {
+                        text.AppendLine();
+
+                        if ( link.Title.HasValue )
+                        {
+                            text.Append( link.Title.Value ).Append( ": " );
+                        }
+
+                        text.Append( link.LinkTarget.OriginalString );
+                    }
+                }
+            }
+        }
+
+        info.Description = text.ToString();
+
+        return info;
+    }
+}
---
+++ examples/AspNetCore/OData/SomeODataOpenApiExample/Program.cs
@@ -0,0 +1,83 @@
+﻿using ApiVersioning.Examples;
+using Asp.Versioning;
+using Asp.Versioning.Conventions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using static Microsoft.AspNetCore.OData.Query.AllowedQueryOptions;
+using static System.Text.Json.JsonNamingPolicy;
+
+var builder = WebApplication.CreateBuilder( args );
+
+// Add services to the container.
+
+// note: this example application intentionally only illustrates the
+// bare minimum configuration for OpenAPI with partial OData support.
+// see the OpenAPI or OData OpenAPI examples for additional options.
+
+builder.Services.Configure<JsonOptions>(
+    options =>
+    {
+        // odata projection operations (ex: $select) use a dictionary, but for good
+        // measure set the default property naming policy for any other use cases
+        options.JsonSerializerOptions.PropertyNamingPolicy = CamelCase;
+        options.JsonSerializerOptions.DictionaryKeyPolicy = CamelCase;
+    } );
+
+builder.Services.AddControllers()
+                .AddOData( options => options.Select() );
+builder.Services.AddApiVersioning()
+                .AddODataApiExplorer(
+                    options =>
+                    {
+                        // add the versioned api explorer, which also adds IApiVersionDescriptionProvider service
+                        // note: the specified format code will format the version as "'v'major[.minor][-status]"
+                        options.GroupNameFormat = "'v'VVV";
+
+                        // configure query options (which cannot otherwise be configured by OData conventions)
+                        options.QueryOptions.Controller<BooksController>()
+                                            .Action( c => c.Get( default ) )
+                                                .Allow( Skip | Count )
+                                                .AllowTop( 100 )
+                                                .AllowOrderBy( "title", "published" );
+                    } );
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
+builder.Services.AddSwaggerGen(
+    options =>
+    {
+        // add a custom operation filter which sets default values
+        options.OperationFilter<SwaggerDefaultValues>();
+
+        var fileName = typeof( Program ).Assembly.GetName().Name + ".xml";
+        var filePath = Path.Combine( AppContext.BaseDirectory, fileName );
+
+        // integrate xml comments
+        options.IncludeXmlComments( filePath );
+    } );
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseSwagger();
+app.UseSwaggerUI(
+    options =>
+    {
+        var descriptions = app.DescribeApiVersions();
+
+        // build a swagger endpoint for each discovered API version
+        foreach ( var description in descriptions )
+        {
+            var url = $"/swagger/{description.GroupName}/swagger.json";
+            var name = description.GroupName.ToUpperInvariant();
+            options.SwaggerEndpoint( url, name );
+        }
+    } );
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
