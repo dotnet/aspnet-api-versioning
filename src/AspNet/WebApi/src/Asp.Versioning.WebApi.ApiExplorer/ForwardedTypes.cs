c27bb0aada829ba1fa48b0c35886451648fc70fa
Chris Martinez <sydefekt@hotmail.com>
2022-11-04 18:33:51 -0700

    Refactor Web API route parsing into core library

---
+++ src/AspNet/WebApi/src/Asp.Versioning.WebApi.ApiExplorer/ForwardedTypes.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+
+using Asp.Versioning.Routing;
+using System.Runtime.CompilerServices;
+
+[assembly: TypeForwardedTo( typeof( IBoundRouteTemplate ) )]
+[assembly: TypeForwardedTo( typeof( IParsedRoute ) )]
+[assembly: TypeForwardedTo( typeof( IPathContentSegment ) )]
+[assembly: TypeForwardedTo( typeof( IPathLiteralSubsegment ) )]
+[assembly: TypeForwardedTo( typeof( IPathParameterSubsegment ) )]
+[assembly: TypeForwardedTo( typeof( IPathSegment ) )]
+[assembly: TypeForwardedTo( typeof( IPathSeparatorSegment ) )]
+[assembly: TypeForwardedTo( typeof( IPathSubsegment ) )]
+[assembly: TypeForwardedTo( typeof( RouteParser ) )]
