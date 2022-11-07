d3b2135564845cf9ac020b574550c6ae1d933fbc
Chris Martinez <sydefekt@hotmail.com>
2022-11-06 19:17:59 -0800

    Add factory to create API version descriptor provider

---
+++ src/AspNetCore/WebApi/src/Asp.Versioning.Http/ApiExplorer/IApiVersionDescriptionProviderFactory.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+
+namespace Asp.Versioning.ApiExplorer;
+
+using Microsoft.AspNetCore.Routing;
+
+/// <summary>
+/// Defines the behavior of a factory used to create a <see cref="IApiVersionDescriptionProvider"/>.
+/// </summary>
+[CLSCompliant( false )]
+public interface IApiVersionDescriptionProviderFactory
+{
+    /// <summary>
+    /// Creates and returns an API version description provider.
+    /// </summary>
+    /// <param name="endpointDataSource">The <see cref="EndpointDataSource">endpoint data
+    /// source</see> used by the provider.</param>
+    /// <returns>A new <see cref="IApiVersionDescriptionProvider">API version description provider</see>.</returns>
+    IApiVersionDescriptionProvider Create( EndpointDataSource endpointDataSource );
+}
