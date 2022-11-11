e11e3a329aafaf8bc2ce6ae64254b1b451e5a051
Chris Martinez <sydefekt@hotmail.com>
2022-11-11 11:03:12 -0800

    Fix code analysis violation after .NET 7 SDK update

--- src/Client/test/Asp.Versioning.Http.Client.Tests/TestServer.cs
+++ src/Client/test/Asp.Versioning.Http.Client.Tests/TestServer.cs
@@ -7,6 +7,7 @@ using System.Net;
 internal sealed class TestServer : HttpMessageHandler
 {
     private readonly HttpResponseMessage response;
+    private bool disposed;
 
     public TestServer() => response = new( HttpStatusCode.OK );
 
@@ -14,4 +15,20 @@ internal sealed class TestServer : HttpMessageHandler
 
     protected override Task<HttpResponseMessage> SendAsync( HttpRequestMessage request, CancellationToken cancellationToken ) =>
         Task.FromResult( response );
+
+    protected override void Dispose( bool disposing )
+    {
+        if ( disposed )
+        {
+            return;
+        }
+
+        base.Dispose( disposing );
+        disposed = true;
+
+        if ( disposing )
+        {
+            response.Dispose();
+        }
+    }
 }
