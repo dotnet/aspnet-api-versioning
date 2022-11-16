85fca0c63a91c1990bd15bd02bb61e4b21684883
Chris Martinez <sydefekt@hotmail.com>
2022-11-16 11:11:20 -0800

    Refactor factories to use injectable delegates

---
+++ src/AspNetCore/WebApi/src/Asp.Versioning.Http/Builder/ApiVersionSetBuilderFactory.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+
+namespace Asp.Versioning.Builder;
+
+/// <summary>
+/// Creates and returns a new API version set builder.
+/// </summary>
+/// <param name="name">The name of the API associated with the builder, if any.</param>
+/// <returns>A new <see cref="ApiVersionSetBuilder">API version set builder</see>.</returns>
+public delegate ApiVersionSetBuilder ApiVersionSetBuilderFactory( string? name = default );
