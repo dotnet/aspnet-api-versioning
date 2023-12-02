234dbc2e8788d398bcee4b9f21cad9dc3bdd9abf
Chris Martinez <sydefekt@hotmail.com>
2023-12-02 11:39:11 -0800

    Add explicit using statements

--- examples/AspNet/WebApi/BasicWebApiExample/Controllers/Values2Controller.cs
+++ examples/AspNet/WebApi/BasicWebApiExample/Controllers/Values2Controller.cs
@@ -1,17 +1,18 @@
 ﻿namespace ApiVersioning.Examples.Controllers;
 
 using Asp.Versioning;
+using System.Net.Http;
 using System.Web.Http;
 
-[ApiVersion( 2.0 )]
-[Route( "api/values" )]
+[ApiVersion(2.0)]
+[Route("api/values")]
 public class Values2Controller : ApiController
 {
     // GET api/values?api-version=2.0
     public IHttpActionResult Get() =>
-        Ok( new
+        Ok(new
         {
             controller = GetType().Name,
             version = Request.GetRequestedApiVersion().ToString(),
-        } );
+        });
 }
