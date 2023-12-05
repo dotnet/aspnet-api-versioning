6060b03a5bca0b2b90d87dbda70b423b05abb143
Chris Martinez <sydefekt@hotmail.com>
2023-12-04 20:47:19 -0800

    Fix formatting

--- examples/AspNet/WebApi/ConventionsWebApiExample/Controllers/Values2Controller.cs
+++ examples/AspNet/WebApi/ConventionsWebApiExample/Controllers/Values2Controller.cs
@@ -3,44 +3,44 @@
 using System.Net.Http;
 using System.Web.Http;
 
-[RoutePrefix("api/values")]
+[RoutePrefix( "api/values" )]
 public class Values2Controller : ApiController
 {
     // GET api/values?api-version=2.0
     [Route]
     public IHttpActionResult Get() =>
-        Ok(new
+        Ok( new
         {
             controller = GetType().Name,
             version = Request.GetRequestedApiVersion().ToString(),
-        });
+        } );
 
     // GET api/values/{id}?api-version=2.0
-    [Route("{id:int}")]
-    public IHttpActionResult Get(int id) =>
-        Ok(new
+    [Route( "{id:int}" )]
+    public IHttpActionResult Get( int id ) =>
+        Ok( new
         {
             controller = GetType().Name,
             id,
             version = Request.GetRequestedApiVersion().ToString(),
-        });
+        } );
 
     // GET api/values?api-version=3.0
     [Route]
     public IHttpActionResult GetV3() =>
-        Ok(new
+        Ok( new
         {
             controller = GetType().Name,
             version = Request.GetRequestedApiVersion().ToString(),
-        });
+        } );
 
     // GET api/values/{id}?api-version=3.0
-    [Route("{id:int}")]
-    public IHttpActionResult GetV3(int id) =>
-        Ok(new
+    [Route( "{id:int}" )]
+    public IHttpActionResult GetV3( int id ) =>
+        Ok( new
         {
             controller = GetType().Name,
             id,
             version = Request.GetRequestedApiVersion().ToString(),
-        });
+        } );
 }
