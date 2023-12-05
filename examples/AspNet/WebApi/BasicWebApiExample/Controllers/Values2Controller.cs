1fbc5999d98fd4066e98d06ca3a7c60b8cefb7b8
Chris Martinez <sydefekt@hotmail.com>
2023-12-04 20:47:19 -0800

    Fix formatting

--- examples/AspNet/WebApi/BasicWebApiExample/Controllers/Values2Controller.cs
+++ examples/AspNet/WebApi/BasicWebApiExample/Controllers/Values2Controller.cs
@@ -4,15 +4,15 @@ using Asp.Versioning;
 using System.Net.Http;
 using System.Web.Http;
 
-[ApiVersion(2.0)]
-[Route("api/values")]
+[ApiVersion( 2.0 )]
+[Route( "api/values" )]
 public class Values2Controller : ApiController
 {
     // GET api/values?api-version=2.0
     public IHttpActionResult Get() =>
-        Ok(new
+        Ok( new
         {
             controller = GetType().Name,
             version = Request.GetRequestedApiVersion().ToString(),
-        });
+        } );
 }
