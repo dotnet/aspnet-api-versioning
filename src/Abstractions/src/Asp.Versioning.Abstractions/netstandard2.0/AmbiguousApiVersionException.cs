1d066a6b724cfe94b27822c1d4059d6acf5cea8d
Chris Martinez <sydefekt@hotmail.com>
2023-12-04 20:03:29 -0800

    Add [Obsolete] for deprecated APIs

--- src/Abstractions/src/Asp.Versioning.Abstractions/netstandard2.0/AmbiguousApiVersionException.cs
+++ src/Abstractions/src/Asp.Versioning.Abstractions/netstandard2.0/AmbiguousApiVersionException.cs
@@ -2,6 +2,7 @@
 
 namespace Asp.Versioning;
 
+using System.ComponentModel;
 using System.Runtime.Serialization;
 
 /// <content>
@@ -10,11 +11,23 @@ using System.Runtime.Serialization;
 [Serializable]
 public partial class AmbiguousApiVersionException : Exception
 {
+    private const string LegacyFormatterImplMessage = "This API supports obsolete formatter-based serialization. It should not be called or extended by application code.";
+#if NET
+    private const string LegacyFormatterImplDiagId = "SYSLIB0051";
+    private const string SharedUrlFormat = "https://aka.ms/dotnet-warnings/{0}";
+#endif
+
     /// <summary>
     /// Initializes a new instance of the <see cref="AmbiguousApiVersionException"/> class.
     /// </summary>
     /// <param name="info">The <see cref="SerializationInfo">serialization info</see> the exception is being deserialized with.</param>
     /// <param name="context">The <see cref="StreamingContext">streaming context</see> the exception is being deserialized from.</param>
+#if NET
+    [Obsolete( LegacyFormatterImplMessage, DiagnosticId = LegacyFormatterImplDiagId, UrlFormat = SharedUrlFormat )]
+#else
+    [Obsolete( LegacyFormatterImplMessage )]
+#endif
+    [EditorBrowsable( EditorBrowsableState.Never )]
     protected AmbiguousApiVersionException( SerializationInfo info, StreamingContext context )
         : base( info, context ) => apiVersions = (string[]) info.GetValue( nameof( apiVersions ), typeof( string[] ) )!;
 
@@ -23,6 +36,10 @@ public partial class AmbiguousApiVersionException : Exception
     /// </summary>
     /// <param name="info">The <see cref="SerializationInfo">serialization info</see> the exception is being serialized with.</param>
     /// <param name="context">The <see cref="StreamingContext">streaming context</see> the exception is being serialized in.</param>
+#if NET
+    [Obsolete( LegacyFormatterImplMessage, DiagnosticId = LegacyFormatterImplDiagId, UrlFormat = SharedUrlFormat )]
+#endif
+    [EditorBrowsable( EditorBrowsableState.Never )]
     public override void GetObjectData( SerializationInfo info, StreamingContext context )
     {
         base.GetObjectData( info, context );
