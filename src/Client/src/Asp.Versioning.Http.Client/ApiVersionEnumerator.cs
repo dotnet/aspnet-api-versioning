4093dc0c98b66b3bb92d5747ef3b145edf416a6e
Chris Martinez <sydefekt@hotmail.com>
2024-03-22 10:09:45 -0700

    Parse versions single header CSVs. Fixes #1070

--- src/Client/src/Asp.Versioning.Http.Client/ApiVersionEnumerator.cs
+++ src/Client/src/Asp.Versioning.Http.Client/ApiVersionEnumerator.cs
@@ -5,14 +5,20 @@
 
 namespace Asp.Versioning.Http;
 
+#if NET
+using System.Buffers;
+#endif
 using System.Collections;
+#if NET
+using static System.StringSplitOptions;
+#endif
 
 /// <summary>
 /// Represents an enumerator of API versions from a HTTP header.
 /// </summary>
 public readonly struct ApiVersionEnumerator : IEnumerable<ApiVersion>
 {
-    private readonly IEnumerable<string> values;
+    private readonly string[] values;
     private readonly IApiVersionParser parser;
 
     /// <summary>
@@ -29,37 +35,72 @@ public readonly struct ApiVersionEnumerator : IEnumerable<ApiVersion>
         ArgumentNullException.ThrowIfNull( response );
         ArgumentException.ThrowIfNullOrEmpty( headerName );
 
-        this.values =
-            response.Headers.TryGetValues( headerName, out var values )
-            ? values
-            : Enumerable.Empty<string>();
-
+        this.values = response.Headers.TryGetValues( headerName, out var values ) ? values.ToArray() : [];
         this.parser = parser ?? ApiVersionParser.Default;
     }
 
     /// <inheritdoc />
     public IEnumerator<ApiVersion> GetEnumerator()
     {
-        using var iterator = values.GetEnumerator();
-
-        if ( !iterator.MoveNext() )
+#if NETSTANDARD
+        for ( var i = 0; i < values.Length; i++ )
         {
-            yield break;
-        }
+            var items = values[i].Split( ',' );
 
-        if ( parser.TryParse( iterator.Current, out var value ) )
-        {
-            yield return value!;
-        }
-
-        while ( iterator.MoveNext() )
-        {
-            if ( parser.TryParse( iterator.Current, out value ) )
+            for ( var j = 0; j < items.Length; j++ )
             {
-                yield return value!;
+                var item = items[j].Trim();
+
+                if ( item.Length > 0 && parser.TryParse( item, out var result ) )
+                {
+                    yield return result!;
+                }
             }
         }
+#else
+        for ( var i = 0; i < values.Length; i++ )
+        {
+            var (count, versions) = ParseVersions( values[i] );
+
+            for ( var j = 0; j < count; j++ )
+            {
+                yield return versions[j];
+            }
+        }
+#endif
     }
 
     IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+#if NET
+    private (int Count, ApiVersion[] Results) ParseVersions( ReadOnlySpan<char> value )
+    {
+        var pool = ArrayPool<Range>.Shared;
+        var ranges = pool.Rent( 5 );
+        var length = value.Split( ranges, ',', RemoveEmptyEntries | TrimEntries );
+
+        while ( length >= ranges.Length )
+        {
+            pool.Return( ranges );
+            length <<= 1;
+            ranges = pool.Rent( length );
+            length = value.Split( ranges, ',', RemoveEmptyEntries | TrimEntries );
+        }
+
+        var results = new ApiVersion[length];
+        var count = 0;
+
+        for ( var i = 0; i < length; i++ )
+        {
+            var text = value[ranges[i]];
+
+            if ( text.Length > 0 && parser.TryParse( text, out var result ) )
+            {
+                results[count++] = result;
+            }
+        }
+
+        pool.Return( ranges );
+        return (count, results);
+    }
+#endif
 }
