51033a9ba82a5140e47aabbbc3a49b452f01b726
Chris Martinez <sydefekt@hotmail.com>
2024-03-25 18:40:29 -0700

    Add parameterless extension method for creating IApiVersionDescriptionProvider

---
+++ src/AspNetCore/WebApi/src/Asp.Versioning.Http/ApiExplorer/IApiVersionDescriptionProviderFactoryExtensions.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+
+namespace Asp.Versioning.ApiExplorer;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Primitives;
+
+/// <summary>
+/// Provides extension methods for <see cref="IApiVersionDescriptionProviderFactory"/>.
+/// </summary>
+[CLSCompliant( false )]
+public static class IApiVersionDescriptionProviderFactoryExtensions
+{
+    /// <summary>
+    /// Creates and returns an API version description provider.
+    /// </summary>
+    /// <param name="factory">The extended <see cref="IApiVersionDescriptionProviderFactory"/>.</param>
+    /// <returns>A new <see cref="IApiVersionDescriptionProvider">API version description provider</see>.</returns>
+    public static IApiVersionDescriptionProvider Create( this IApiVersionDescriptionProviderFactory factory )
+    {
+        ArgumentNullException.ThrowIfNull( factory );
+        return factory.Create( new EmptyEndpointDataSource() );
+    }
+
+    private sealed class EmptyEndpointDataSource : EndpointDataSource
+    {
+        public override IReadOnlyList<Endpoint> Endpoints { get; } = [];
+
+        public override IChangeToken GetChangeToken() => new CancellationChangeToken( CancellationToken.None );
+
+        public override IReadOnlyList<Endpoint> GetGroupedEndpoints( RouteGroupContext context ) => Endpoints;
+    }
+}
