6c99cc1f148e89d66b740f7f8ffb53b02bf586a1
Matthias Gessinger <matthias@gessinger.de>
2025-11-26 18:55:34 +0100

    Implement DeprecationPolicyManager

---
+++ src/Abstractions/src/Asp.Versioning.Abstractions/IDeprecationPolicyManager.cs
@@ -0,0 +1,9 @@
+// Copyright (c) .NET Foundation and contributors. All rights reserved.
+
+namespace Asp.Versioning;
+
+/// <summary>
+/// Defines the behavior of an API version deprecation policy manager.
+/// </summary>
+public interface IDeprecationPolicyManager : IPolicyManager<DeprecationPolicy>
+{ }
