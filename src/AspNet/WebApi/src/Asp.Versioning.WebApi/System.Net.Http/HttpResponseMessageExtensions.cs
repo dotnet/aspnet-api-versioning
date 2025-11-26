3de93edfb9be0102ef26aaad80c4a7e880013981
Matthias Gessinger <matthias@gessinger.de>
2025-11-26 19:34:17 +0100

    Emit Deprecation header when policy is set

--- src/AspNet/WebApi/src/Asp.Versioning.WebApi/System.Net.Http/HttpResponseMessageExtensions.cs
+++ src/AspNet/WebApi/src/Asp.Versioning.WebApi/System.Net.Http/HttpResponseMessageExtensions.cs
@@ -13,7 +13,10 @@ using System.Net.Http.Headers;
 public static class HttpResponseMessageExtensions
 {
     private const string Sunset = nameof( Sunset );
+    private const string Deprecation = nameof( Deprecation );
     private const string Link = nameof( Link );
+
+    private static readonly DateTime unixEpoch = new DateTime( 1970, 1, 1 );
 
     /// <summary>
     /// Writes the sunset policy to the specified HTTP response.
@@ -33,6 +36,35 @@ public static class HttpResponseMessageExtensions
         }
 
         AddLinkHeaders( headers, sunsetPolicy.Links );
+    }
+
+    /// <summary>
+    /// Writes the sunset policy to the specified HTTP response.
+    /// </summary>
+    /// <param name="response">The <see cref="HttpResponseMessage">HTTP response</see> to write to.</param>
+    /// <param name="deprecationPolicy">The <see cref="DeprecationPolicy">deprecation policy</see> to write.</param>
+    public static void WriteDeprecationPolicy( this HttpResponseMessage response, DeprecationPolicy deprecationPolicy )
+    {
+        ArgumentNullException.ThrowIfNull( response );
+        ArgumentNullException.ThrowIfNull( deprecationPolicy );
+
+        var headers = response.Headers;
+
+        if ( deprecationPolicy.Date.HasValue )
+        {
+            long unixTimestamp;
+            DateTimeOffset deprecationDate = deprecationPolicy.Date.Value;
+
+#if NETFRAMEWORK
+            unixTimestamp = (int) deprecationDate.Subtract( unixEpoch ).TotalSeconds;
+#else
+            unixTimestamp = deprecationDate.ToUnixTimeSeconds();
+#endif
+
+            headers.Add( Deprecation, $"@{unixTimestamp}" );
+        }
+
+        AddLinkHeaders( headers, deprecationPolicy.Links );
     }
 
     private static void AddLinkHeaders( HttpResponseHeaders headers, IList<LinkHeaderValue> links )
