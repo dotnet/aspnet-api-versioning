2d7a771b8a007f28d77303e2eb3c148ae9e40612
Chris Martinez <sydefekt@hotmail.com>
2026-02-09 08:11:08 -0800

    Add OpenAPI test project

---
+++ src/AspNetCore/WebApi/test/Asp.Versioning.OpenApi.Tests/OpenApiDocumentDescriptionOptionsTest.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+
+
+namespace Asp.Versioning.OpenApi;
+
+public class OpenApiDocumentDescriptionOptionsTest
+{
+    [Fact]
+    public void sunset_notice_should_be_null_without_a_date()
+    {
+        // arrange
+        var options = new OpenApiDocumentDescriptionOptions();
+        var policy = new SunsetPolicy();
+
+        // act
+        var actual = options.SunsetNotice( policy );
+
+        // assert
+        actual.Should().BeNull();
+    }
+
+    [Fact]
+    public void sunset_notice_should_return_expected_message()
+    {
+        // arrange
+        var expected = "The API will be sunset on 2/8/2026.";
+        var options = new OpenApiDocumentDescriptionOptions();
+        var date = new DateTimeOffset( new DateTime( 2026, 2, 8 ) );
+        var policy = new SunsetPolicy( date );
+
+        // act
+        var actual = options.SunsetNotice( policy );
+
+        // assert
+        actual.Should().Be( expected );
+    }
+}
