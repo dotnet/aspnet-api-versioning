48538e45b2e340a489294549f1f9e8a188e0ea98
Chris Martinez <sydefekt@hotmail.com>
2026-04-19 09:50:46 -0700

    Remove used types load via Reflection

--- src/AspNetCore/WebApi/src/Asp.Versioning.OpenApi/Reflection/Type.cs
+++ src/AspNetCore/WebApi/src/Asp.Versioning.OpenApi/Reflection/Type.cs
@@ -16,8 +16,6 @@ internal static class Type
 
     public static readonly System.Type IEnumerableOfNamedService = System.Type.GetType( "System.Collections.Generic.IEnumerable`1[[Microsoft.AspNetCore.OpenApi.NamedService`1[[Microsoft.AspNetCore.OpenApi.OpenApiDocumentService, Microsoft.AspNetCore.OpenApi]], Microsoft.AspNetCore.OpenApi]], System.Private.CoreLib", throwOnError: true )!;
 
-    public static readonly System.Type ListOfNamedService = System.Type.GetType( "System.Collections.Generic.List`1[[Microsoft.AspNetCore.OpenApi.NamedService`1[[Microsoft.AspNetCore.OpenApi.OpenApiDocumentService, Microsoft.AspNetCore.OpenApi]], Microsoft.AspNetCore.OpenApi]], System.Private.CoreLib", throwOnError: true )!;
-
     [DynamicallyAccessedMembers( PublicConstructors )]
     public static readonly System.Type OpenApiDocumentProvider = System.Type.GetType( "Microsoft.Extensions.ApiDescriptions.OpenApiDocumentProvider, Microsoft.AspNetCore.OpenApi", throwOnError: true )!;
 
@@ -26,7 +24,4 @@ internal static class Type
 
     [DynamicallyAccessedMembers( PublicConstructors )]
     public static readonly System.Type OpenApiSchemaService = System.Type.GetType( "Microsoft.AspNetCore.OpenApi.OpenApiSchemaService, Microsoft.AspNetCore.OpenApi", throwOnError: true )!;
-
-    [DynamicallyAccessedMembers( PublicConstructors )]
-    public static readonly System.Type OpenApiSchemaJsonOptions = System.Type.GetType( "Microsoft.AspNetCore.OpenApi.OpenApiSchemaJsonOptions, Microsoft.AspNetCore.OpenApi", throwOnError: true )!;
 }
