d5bded5ec893023fa626f73731da9fd46a229dec
Chris Martinez <sydefekt@hotmail.com>
2026-07-31 16:23:26 -0700

    Finalize annotation and visibility design

--- src/Abstractions/src/Asp.Versioning.Abstractions/IAnnotation.cs
+++ src/Abstractions/src/Asp.Versioning.Abstractions/IAnnotation.cs
@@ -3,17 +3,20 @@
 namespace Asp.Versioning;
 
 /// <summary>
-/// Defines the behavior of a member filter.
+/// Defines the behavior of an annotation.
 /// </summary>
-/// <typeparam name="T">The type of member.</typeparam>
-public interface IMemberFilter<T>
+/// <typeparam name="TSource">The type of annotated source.</typeparam>
+/// <typeparam name="TAnnotation">The type of annotation.</typeparam>
+/// <remarks>An annotation describes a source; it does not act on it. An action taken from an annotation, such as
+/// deciding whether a data member is visible, is layered on top of the annotations reported here. A source can be
+/// anything that is annotated, which includes, but is not limited to, a data member.</remarks>
+public interface IAnnotation<in TSource, TAnnotation>
 {
     /// <summary>
-    /// Determines whether the provide member is visible to the specified API version.
+    /// Attempts to retrieve the annotation for the specified source.
     /// </summary>
-    /// <param name="member">The member to evaluate.</param>
-    /// <param name="apiVersion">The <see cref="ApiVersion">API version</see> to compare against.</param>
-    /// <returns>True if the member is visible in the specified <paramref name="apiVersion">API version</paramref>;
-    /// otherwise, false.</returns>
-    bool IsVisible( T member, ApiVersion apiVersion );
+    /// <param name="source">The source to retrieve the annotation for.</param>
+    /// <param name="annotation">The retrieved <typeparamref name="TAnnotation">annotation</typeparamref>, if any.</param>
+    /// <returns>True if the <paramref name="source"/> is annotated; otherwise, false.</returns>
+    bool TryGet( TSource source, [MaybeNullWhen( false )] out TAnnotation annotation );
 }
