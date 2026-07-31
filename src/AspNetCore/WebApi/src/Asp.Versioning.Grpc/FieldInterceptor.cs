7033ab3a1933631d0ace46c24031cc010310ce5f
Chris Martinez <sydefekt@hotmail.com>
2026-07-31 16:28:38 -0700

    Refactor to annotation design and close over-posting gap

--- src/AspNetCore/WebApi/src/Asp.Versioning.Grpc/FieldInterceptor.cs
+++ src/AspNetCore/WebApi/src/Asp.Versioning.Grpc/FieldInterceptor.cs
@@ -1,15 +1,15 @@
-﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Copyright (c) .NET Foundation and contributors. All rights reserved.
 
 #pragma warning disable CA1812
 
 namespace Asp.Versioning;
 
-using Google.Protobuf;
 using Google.Protobuf.Reflection;
 using Grpc.Core;
 using Grpc.Core.Interceptors;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics.CodeAnalysis;
 
 internal sealed class FieldInterceptor : Interceptor
 {
@@ -18,44 +18,101 @@ internal sealed class FieldInterceptor : Interceptor
         ServerCallContext context,
         UnaryServerMethod<TRequest, TResponse> continuation )
     {
+        if ( !TryResolve( context, out var annotations, out var apiVersion ) )
+        {
+            return await continuation( request, context ).ConfigureAwait( false );
+        }
+
+        MessageFields.Validate( annotations, request, apiVersion );
+
         var response = await continuation( request, context ).ConfigureAwait( false );
 
-        if ( response is not IMessage message )
-        {
-            return response;
-        }
-
-        var http = context.GetHttpContext();
-        var feature = http.ApiVersioningFeature;
-
-        if ( feature.RequestedApiVersion is not { } apiVersion )
-        {
-            return response;
-        }
-
-        var filter = http.RequestServices.GetRequiredService<IMemberFilter<FieldDescriptor>>();
-
-        FilterFields( filter, message, apiVersion );
+        MessageFields.Filter( annotations, response, apiVersion );
 
         return response;
     }
 
-    private static void FilterFields( IMemberFilter<FieldDescriptor> filter, IMessage message, ApiVersion apiVersion )
+    public override Task<TResponse> ClientStreamingServerHandler<TRequest, TResponse>(
+        IAsyncStreamReader<TRequest> requestStream,
+        ServerCallContext context,
+        ClientStreamingServerMethod<TRequest, TResponse> continuation )
     {
-        var descriptor = message.Descriptor;
-
-        foreach ( var field in descriptor.Fields.InDeclarationOrder() )
+        if ( !TryResolve( context, out var annotations, out var apiVersion ) )
         {
-            if ( !filter.IsVisible( field, apiVersion ) )
-            {
-                field.Accessor.Clear( message );
-                continue;
-            }
-
-            if ( field.FieldType == FieldType.Message && field.Accessor.GetValue( message ) is IMessage nested )
-            {
-                FilterFields( filter, nested, apiVersion );
-            }
+            return continuation( requestStream, context );
         }
+
+        return Filtered( requestStream, context, continuation, annotations, apiVersion );
+
+        static async Task<TResponse> Filtered(
+            IAsyncStreamReader<TRequest> requestStream,
+            ServerCallContext context,
+            ClientStreamingServerMethod<TRequest, TResponse> continuation,
+            IAnnotation<FieldDescriptor, ApiVersionRange> annotations,
+            ApiVersion apiVersion )
+        {
+            var reader = new ValidatingStreamReader<TRequest>( requestStream, annotations, apiVersion );
+            var response = await continuation( reader, context ).ConfigureAwait( false );
+
+            MessageFields.Filter( annotations, response, apiVersion );
+
+            return response;
+        }
+    }
+
+    public override Task ServerStreamingServerHandler<TRequest, TResponse>(
+        TRequest request,
+        IServerStreamWriter<TResponse> responseStream,
+        ServerCallContext context,
+        ServerStreamingServerMethod<TRequest, TResponse> continuation )
+    {
+        if ( !TryResolve( context, out var annotations, out var apiVersion ) )
+        {
+            return continuation( request, responseStream, context );
+        }
+
+        MessageFields.Validate( annotations, request, apiVersion );
+
+        var writer = new FilteringStreamWriter<TResponse>( responseStream, annotations, apiVersion );
+
+        return continuation( request, writer, context );
+    }
+
+    public override Task DuplexStreamingServerHandler<TRequest, TResponse>(
+        IAsyncStreamReader<TRequest> requestStream,
+        IServerStreamWriter<TResponse> responseStream,
+        ServerCallContext context,
+        DuplexStreamingServerMethod<TRequest, TResponse> continuation )
+    {
+        if ( !TryResolve( context, out var annotations, out var apiVersion ) )
+        {
+            return continuation( requestStream, responseStream, context );
+        }
+
+        var reader = new ValidatingStreamReader<TRequest>( requestStream, annotations, apiVersion );
+        var writer = new FilteringStreamWriter<TResponse>( responseStream, annotations, apiVersion );
+
+        return continuation( reader, writer, context );
+    }
+
+    // a call that did not resolve an API version is passed through untouched. there is no version to compare a
+    // field against, so no field can be shown to be out of range
+    private static bool TryResolve(
+        ServerCallContext context,
+        [NotNullWhen( true )] out IAnnotation<FieldDescriptor, ApiVersionRange>? annotations,
+        [NotNullWhen( true )] out ApiVersion? apiVersion )
+    {
+        if ( context.GetHttpContext() is not { } http ||
+             http.ApiVersioningFeature.RequestedApiVersion is not { } version )
+        {
+            annotations = default;
+            apiVersion = default;
+            return false;
+        }
+
+        annotations = http.RequestServices.GetRequiredService<IAnnotation<FieldDescriptor, ApiVersionRange>>();
+        apiVersion = version;
+
+        return true;
     }
 }
