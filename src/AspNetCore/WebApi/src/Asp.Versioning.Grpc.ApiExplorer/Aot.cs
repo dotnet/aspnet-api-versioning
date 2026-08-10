db13e5e265161f400602f724be91c9f47ce87a38
Chris Martinez <sydefekt@hotmail.com>
2026-08-10 13:15:12 -0700

    Delete dead, unused file

--- src/AspNetCore/WebApi/src/Asp.Versioning.Grpc.ApiExplorer/Aot.cs
+++
@@ -1,8 +0,0 @@
-﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
-
-namespace Asp.Versioning;
-
-internal static class Aot
-{
-    internal const string TrimmingMessage = "The API Explorer does not currently support trimming or native AOT. https://aka.ms/aspnet/trimming";
-}
